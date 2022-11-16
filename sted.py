ee5227c03d70584d5ea6feea077906669a0f179f
piehunter123 <34416605+piehunter123@users.noreply.github.com>
2022-11-16 14:14:54 +0100

    Added save/load

--- sted.py
+++ sted.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 16 12:27:40 2022
+
+@author: PieHu
+"""
+
+mineSteder={}
+    
 # oppgave G
 class Sted: 
     def __init__(self,id, navn,gatenavn, postnummer, poststed): 
@@ -10,18 +19,29 @@ class Sted:
     def __str__(self):
         return f'Sted({self.id}, {self.navn}, {self.gatenavn},{self.postnummer},{self.poststed}'
 
+mineSteder["Ikke angitt"]=Sted("Ikke angitt",0,0,0,0)
 
 # Oppgave H  
-def bruker_sted(): 
-     info = Sted(input("id: "), input("navn: "), input("gatenavn: "), input("postnummer: "), input("poststed: "))
-     return info
+def lagNyttSted():
+     id=input("id: ")
+     sted = Sted(id, input("navn: "), input("gatenavn: "), input("postnummer: "), input("poststed: "))
+     mineSteder[id]=sted
  
-#Oppgave I
-mineSteder = []
-def lagreStedTilFil():
-  result = ""
-  for k in mineSteder:
-      result += f'({k._id};{k.navn}; {k.gatenavn};{k.postnummer};{k.poststed};\n)'
-  f = open(mineSteder, "w")
+'''
+Format på sted fil:
+    id;navn;gatenavn;postnummer;poststed
+'''
+def lastInnStederFraFil(fildestinasjon):
+  steder=open(fildestinasjon).read().split('\n')
+  for i in range(len(steder)):
+    verdi=steder[i].split(';')
+    if(len(verdi)<5): continue #ikke last inn tomme/ufullstendige steder
+    mineSteder[verdi[1]]=Sted(verdi[0],verdi[1],verdi[2],verdi[3],verdi[4], )
+
+def lagreStederTilFil(fildestinasjon):
+  result=""
+  for s in mineSteder:
+      result+=f"{mineSteder[s].id};{mineSteder[s].navn};{mineSteder[s].gatenavn};{mineSteder[s].postnummer};{mineSteder[s].poststed}\n"
+  f = open(fildestinasjon, "w")
   f.write(result)
   f.close()
