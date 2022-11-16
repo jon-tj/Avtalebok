8db4e0d85bac074baf1c4e4e41818a3fd54bc77d
Agotnes <116582829+Agotnes@users.noreply.github.com>
2022-11-16 13:05:33 +0100

    Update sted.py

--- sted.py
+++ sted.py
@@ -1,14 +1,14 @@
 # oppgave G
 class Sted: 
-    def __init__(self,_id, navn,gatenavn, postnummer, poststed): 
-        self._id = _id
+    def __init__(self,id, navn,gatenavn, postnummer, poststed): 
+        self.id = id
         self.navn = navn
         self.gatenavn = gatenavn
         self.postnummer = postnummer
         self.poststed = poststed
         
     def __str__(self):
-        return f'Sted({self._id}, {self.navn}, {self.gatenavn},{self.postnummer},{self.poststed}'
+        return f'Sted({self.id}, {self.navn}, {self.gatenavn},{self.postnummer},{self.poststed}'
 
 
 # Oppgave H  
